fcd18f337ca1e564c02c5ec893f95a0a772c824a
phantuananh18 <tuananh.61779@gmail.com>
2021-10-04 11:30:02 +0700

    first commit

---
+++ DemoFlyweightPattern/DemoFlyweightPattern/BBQKing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFlyweightPattern
+{
+    /// <summary>
+    /// A  ConcreteFlyweight class
+    /// </summary>
+    class BBQKing :Slider
+    {
+        public BBQKing()
+        {
+            Name = "BBQ King";
+            Cheese = "American";
+            Toppings = "Onion rings, lettuce, and BBQ sauce";
+            Price = 2.49m;
+        }
+
+        public override void Display(int orderTotal)
+        {
+            Console.WriteLine("Slider #" + orderTotal + ": "
+                              + Name + " - topped with "
+                              + Cheese + " cheese and "
+                              + Toppings + "! $" + Price.ToString());
+        }
+    }
+}
